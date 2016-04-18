b1a0a00f118858eec984aba1a3446f7c433e35ec
nchlodzi <nchlodzi@gmail.com>
2016-04-18 05:04:31 +0200

    first hovers

--- js/app.js
+++ js/app.js
@@ -1,30 +1,54 @@
 $(document).ready(function(){
 
-
-$('.navigation').on('click', function(){
-	if(window.matchMedia('max-width: 360px')){
-		$('.navigation li').toggleClass('show');
-		console.log("działą")
-		
-		
-	}
-
+$('.computer').on('mouseover',function(){
+	$('.bgTurq1').css('display', 'block');
 })
 
-/*
-function now (){
-	window.addEventListener('resize', function(event){
-		var mg = window.matchMedia("(max-width: 500px)");
+$('.computer').on('mouseleave',function(){
+	$('.bgTurq1').css('display', 'none');
+})
 
-		if(mg.matches){
-			cosnole.log('yes');
-		}else{
-			cosnole.log('no');
-				}
-			}
-		}
-	})
-}
-*/
+$('.headphones').on('mouseover',function(){
+	$('.bgTurq2').css('display', 'block');
+})
+
+$('.headphones').on('mouseleave',function(){
+	$('.bgTurq2').css('display', 'none');
+})
+
+$('.desk').on('mouseover',function(){
+	$('.bgTurq3').css('display', 'block');
+})
+
+$('.desk').on('mouseleave',function(){
+	$('.bgTurq3').css('display', 'none');
+})
+
+
+$('.firstTeamPerson').on('mouseover',function(){
+	$('.bgTurqBig1').css('display', 'block');
+})
+
+$('.firstTeamPerson').on('mouseleave',function(){
+	$('.bgTurqBig1').css('display', 'none');
+})
+
+$('.secondTeamPerson').on('mouseover',function(){
+	$('.bgTurqBig2').css('display', 'block');
+})
+
+$('.secondTeamPerson').on('mouseleave',function(){
+	$('.bgTurqBig2').css('display', 'none');
+})
+
+$('.thirdTeamPerson').on('mouseover',function(){
+	$('.bgTurqBig3').css('display', 'block');
+})
+
+$('.thirdTeamPerson').on('mouseleave',function(){
+	$('.bgTurqBig3').css('display', 'none');
+})
+
+
 });
 
