6c6b11092c5fdb19e7a3a8f1d12debfe1b6be070
nchlodzi <nchlodzi@gmail.com>
2016-04-20 05:08:04 +0200

    begining of responsivnes

--- js/app.js
+++ js/app.js
@@ -25,21 +25,42 @@ $('.desk').on('mouseleave',function(){
 })
 
 
-$('.firstTeamPerson').on('mouseover',function(){
-	$('.bgTurqBig1').css('display', 'block');
-})
+// $('.firstTeamPerson').hover(function(){
+// 	$('.bgTurqBig1').css('display', 'block');
+// 	console.log('tak');
+// 	$('.yellowRectangle').css('display', 'block');
+// 	console.log('nie');
+// })
 
-$('.firstTeamPerson').on('mouseleave',function(){
-	$('.bgTurqBig1').css('display', 'none');
-})
+// $('.yellowRectangle').find('div .col-3').on('mouseover', function(){
+// 	$(this).css('background-color', '#f38181');
+// })
 
-$('.secondTeamPerson').on('mouseover',function(){
-	$('.bgTurqBig2').css('display', 'block');
-})
+$('.yellowRectangle').hover(function(){
+	// $(this).prev().removeClass();
+	$(this).prev().addClass('hover');
+	console.log('mama');
+},
+ function(){
+	$(this).prev().removeClass('hover');
+	// $(this).prev().addClass('.firstTeamPerson');
+	console.log('tata');
+});
 
-$('.secondTeamPerson').on('mouseleave',function(){
-	$('.bgTurqBig2').css('display', 'none');
-})
+
+
+// $('.firstTeamPerson').on('mouseleave',function(){
+// 	$('.bgTurqBig1').css('display', 'none');
+// 	$('.yellowRectangle').css('display', 'none');
+// })
+
+// $('.secondTeamPerson').on('mouseover',function(){
+// 	$('.bgTurqBig2').css('display', 'block');
+// })
+
+// $('.secondTeamPerson').on('mouseleave',function(){
+// 	$('.bgTurqBig2').css('display', 'none');
+// })
 
 $('.thirdTeamPerson').on('mouseover',function(){
 	$('.bgTurqBig3').css('display', 'block');
@@ -48,6 +69,46 @@ $('.thirdTeamPerson').on('mouseover',function(){
 $('.thirdTeamPerson').on('mouseleave',function(){
 	$('.bgTurqBig3').css('display', 'none');
 })
+
+
+
+	function accordion(){
+
+		$('.accordion p:not(:first)').hide();
+		$('.accordion h1').on('click', function(){
+			$(this).next('p').slideToggle().siblings('p:visible').slideUp();
+			$(this).children('.arrow').toggle();
+			$(this).children('.arrowSecond').toggle();
+		})
+
+
+	}
+
+	accordion();
+
+
+$('.secondTeamPerson').hover(function(){
+	$('.hoverPhoto').show();
+},
+ function(){
+	$('.hoverPhoto').hide();
+
+});
+
+
+$('.secondTeamPerson .facebook').hover(function(){
+	$(this).css('background-color', '#f38181');
+},
+ function(){
+	$(this).css('background-color', '#fce389');
+
+});
+
+
+	$('.topPeople').find('.personName2').on('click', function(){
+		$(this).siblings('.peopleP').slideToggle();
+	})
+
 
 
 });
